0aa615d54cb2e03beb642f5a7b2f06bf491c10bb
rsshonjoydas <rsshonjoydas@gmail.com>
2021-09-15 19:28:02 +0600

    Partials & @import

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
 function buildsStyles() {
-  return src('index.scss')
+  return src('*.scss')
     .pipe(sass())
     .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['index.scss'], buildsStyles)
+  watch(['*.scss'], buildsStyles)
 }
 
 exports.default = series(buildsStyles, watchTask)
