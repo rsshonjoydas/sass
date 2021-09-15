5d46a23e18e074aa3170af78b6d4b5530715821b
rsshonjoydas <rsshonjoydas@gmail.com>
2021-09-15 21:08:42 +0600

    Nested Rules

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
 function buildsStyles() {
-  return src('*.scss')
+  return src('styles/**/*.scss')
     .pipe(sass())
     .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['*.scss'], buildsStyles)
+  watch(['styles/**/*.scss'], buildsStyles)
 }
 
 exports.default = series(buildsStyles, watchTask)
