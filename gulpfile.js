98ce559e6f10e3603c1d34691e23a99667e9e599
rsshonjoydas <rsshonjoydas@gmail.com>
2021-09-15 19:10:14 +0600

    compiling sass

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const { src, dest, watch, series } = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+
+function buildsStyles() {
+  return src('index.scss')
+    .pipe(sass())
+    .pipe(dest('css'))
+}
+
+function watchTask() {
+  watch(['index.scss'], buildsStyles)
+}
+
+exports.default = series(buildsStyles, watchTask)
