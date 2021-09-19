41a7936d180fb44cf4c6fe84900b47bb91930447
rsshonjoydas <rsshonjoydas@gmail.com>
2021-09-19 08:28:19 +0600

    Customizing the Library

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const sass = require('gulp-sass')(require('sass'))
 const purgecss = require('gulp-purgecss')
 
 function buildsStyles() {
-  return src('styles/**/*.scss')
+  return src('sass/**/*.scss')
     .pipe(sass())
     .pipe(purgecss({ content: ['*.html'] }))
     .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['styles/**/*.scss', '*.html'], buildsStyles)
+  watch(['sass/**/*.scss', '*.html'], buildsStyles)
 }
 
 exports.default = series(buildsStyles, watchTask)
